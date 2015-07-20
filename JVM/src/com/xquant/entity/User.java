4e93729bd362d19383ff48c19cce70ad34932a6e
w3c <907277677@qq.com>
2015-07-20 20:06:25 +0800

    user

---
+++ JVM/src/com/xquant/entity/User.java
@@ -0,0 +1,50 @@
+package com.xquant.entity;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String realname;
+	private String sex;
+	private String phone;
+	private int age;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
